ce08e93944aa40a5c59fc79deea267144748ae7e
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-01-20 15:15:11 +0300

    Implemented the rest API with MVC software architcture

---
+++ app.js
@@ -0,0 +1,27 @@
+const express  = require('express')
+const morgan  = require('morgan')
+
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
+
+const app = express()
+// Middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next()
+})
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next()
+})
+
+
+
+
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
+
+
+module.exports = app;
