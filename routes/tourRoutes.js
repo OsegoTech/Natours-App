dad83b75fc1ae700ff490156c65b9020c119537f
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-01-30 12:41:32 +0300

    aggregation Pipeline, matching and grouping

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,8 +5,13 @@ const router = express.Router();
 // router.param('id', tourController.checkID)
 
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours)
 
+router
+  .route('/tour-stats')
+  .get(tourController.getTourStats)
 
 router
   .route('/')
