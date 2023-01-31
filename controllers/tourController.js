6c416a15db88c207484d2ecd92d3e79e02cb8f16
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-01-31 14:13:56 +0300

    Aggregation pipeline: unwinding and projecting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -147,7 +147,59 @@ exports.getTourStats = async (req, res) => {
       }
     })
   }catch (err) {
-    res.status(400).json({
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid data sent'
+    });
+  }
+}
+
+exports.getMonthlyPlan = async (req, res) => {
+  try{
+    const year = req.params.year * 1
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new  Date(`${year}-01-01`),
+            $lte: new  Date(`${year}-12-31`),
+
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates'},
+          numTourStarts: { $sum: 1},
+          tours: { $push: '$name'}
+        }
+      },
+      {
+        $addFields: { month: '_$id'}
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      },
+      {
+        $sort: { numTourStarts: -1 }
+      },
+      {
+        $limit: 12
+      }
+    ])
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan
+      }
+    })
+  }catch (err) {
+    res.status(404).json({
       status: 'fail',
       message: 'Invalid data sent'
     });
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -14,6 +14,10 @@ router
   .get(tourController.getTourStats)
 
 router
+  .route('/monthly-plan/:year')
+  .get(tourController.getMonthlyPlan)
+
+router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour)
