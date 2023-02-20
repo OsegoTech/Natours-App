08eaf70bef9de407032f209e83d5565f7b00edd3
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-02-20 10:31:57 +0300

    added built-in data validators to my tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,9 @@ const tourSchema = new mongoose.Schema({
     type:String,
     required: [true, 'Tour must have a name'],
     unique: true,
-    trim: true
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+    minlength: [10, 'A tour name must have more or equal than 10 characters']
   },
   slug: String,
   duration: {
@@ -19,11 +21,17 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String,
-    required: [true, 'A tour must have a difficulty']
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either easy, medium or difficult'
+    }
   },
   ratingsAverage: {
     type: Number,
-    default: 4.5
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must equal to or below  5.0']
   },
   ratingsQuantity: {
     type: Number,
