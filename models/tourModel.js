d60dfa59866c277f9f6046d5228833794c707067
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-02-20 10:21:12 +0300

    Aggregation middlewares

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
     unique: true,
     trim: true
   },
+  slug: String,
   duration: {
     type: Number,
     required: [true, 'A tour must have a duration']
@@ -51,7 +53,54 @@ const tourSchema = new mongoose.Schema({
     default: Date.now(),
     select: false
   },
-  startDates: [Date]
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  toJSON: { virtuals: true},
+  toObject: { virtuals: true}
+})
+
+tourSchema.virtual('durationWeeks').get(function() {
+  return this.duration / 7
+})
+
+// Document middleware runs before the .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, {lower: true})
+  next()
+})
+
+// tourSchema.pre('save', function(next) {
+//   console.log('Will save document....');
+//   next()
+// })
+//
+// tourSchema.post('save', function(doc, next) {
+//   console.log(doc);
+//   next()
+// })
+
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true}})
+
+  this.start = Date.now()
+  next()
+})
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  // console.log(docs);
+  next()
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true}}})
+  console.log(this.pipeline());
+  next()
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
