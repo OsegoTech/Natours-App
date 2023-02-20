bde81940a3689edc6eeb7fe082f46509f7cd8abf
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-02-20 12:28:10 +0300

    Handling unhandled routes in our application

--- app.js
+++ app.js
@@ -15,19 +15,21 @@ app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next()
-})
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next()
 })
 
-
-
-
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+
+// Handling unhandled routes in our app
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server! `
+  })
+})
 
 
 module.exports = app;
