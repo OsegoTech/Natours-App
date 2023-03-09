c0f8f1d6c8437b69d43674b7d2553b52f3cc1345
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-03-09 21:14:53 +0300

    Catching uncaught exceptions

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION  SHUTTING DOWN....');
+  console.log(err.name, err.message);
+  process.exit(1)
+})
+
 dotenv.config({path: './config.env'})
 const app = require('./app')
 
@@ -22,9 +28,11 @@ const server = app.listen(port, () => {
 })
 
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION  SHUTTING DOWN....');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1)
   })
 })
+
+
