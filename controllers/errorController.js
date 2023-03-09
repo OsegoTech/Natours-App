c1c75e9c327ac50bbac35764c389a43b10b0ac6f
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-03-09 20:49:32 +0300

    Hnadling Mongoose Validation Errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -14,6 +14,12 @@ const handleDuplicateFieldsDB = err => {
   return new AppError(message, 400)
 }
 
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -57,6 +63,7 @@ module.exports = ((err, req, res, next) => {
 
     if (error.name === 'CastError') error =  handleCastErrorDB(error)
     if (error.code === 11000) error = handleDuplicateFieldsDB(error)
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
     sendErrorProd(error, res)
   }
 
