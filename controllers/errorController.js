189f7e5e5b6ab85dc0cbd8831ead64a1611966e7
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-03-09 20:37:16 +0300

    Hnadling database duplicate fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,19 @@
+// Regular expressions match text between quotes
+
+const AppError = require('./../utils/appError')
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`
+  return new AppError(message, 400)
+}
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value`
+  return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -37,7 +53,11 @@ module.exports = ((err, req, res, next) => {
   if (process.env.NODE_ENV === 'development'){
     sendErrorDev(err, res)
   } else if (process.env.NODE_ENV === 'production'){
-    sendErrorProd(err, res)
+    let error = {...err};
+
+    if (error.name === 'CastError') error =  handleCastErrorDB(error)
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error)
+    sendErrorProd(error, res)
   }
 
 
