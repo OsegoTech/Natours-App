c6921ed831aff9f6def592bf62fb9966befad47d
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-03-09 20:59:09 +0300

    Errors Outside Express: Unhandled Rejections

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ mongoose.connect(DB, {
 
 // Create an instance of server
 const port = process.env.PORT || 3000
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}.....`);
+})
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION  SHUTTING DOWN....');
+  server.close(() => {
+    process.exit(1)
+  })
 })
