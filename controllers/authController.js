cf875d115ed88a4f2f4c23321c5465c1d9fb663b
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-03-20 15:45:10 +0300

    Changed freshUser to CurrentUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,17 +66,17 @@ exports.protect = catchAsync(async (req, res, next) => {
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3). Check if user still exists
-  const freshUser = await User.findById(decoded.id)
-  if (!freshUser){
+  const currentUser = await User.findById(decoded.id)
+  if (!currentUser){
     return next(new AppError('The user belonging to the token nolonger exists', 401))
   }
 
   // 4). Check if user changed password after the JWT was issued]
- if (freshUser.changedPasswordAfter(decoded.iat)){
+ if (currentUser.changedPasswordAfter(decoded.iat)){
    return next(new  AppError('User recently changed password, Please log in again', 401))
  }
 
   // Grant access to protected route
-  req.use = freshUser
+  req.use = currentUser
   next()
 })
