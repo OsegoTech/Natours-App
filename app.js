10a673318e10aa7d1d4f1809468bed4818e37dd7
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-07-01 21:44:26 +0300

    Authentication, Authorization and Security complete

--- app.js
+++ app.js
@@ -1,37 +1,73 @@
-const express  = require('express')
-const morgan  = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const hemlet = require('helmet');
+const expressMongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
-const globalErrorHandler = require('./controllers/errorController')
-const AppError = require('./utils/appError')
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const globalErrorHandler = require('./controllers/errorController');
+const AppError = require('./utils/appError');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
-const app = express()
+const app = express();
 // Middlewares
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'))
-}
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.static(`${__dirname}/public`))
+// Set security HTTP headers
+app.use(hemlet());
 
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Limit request from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!',
+});
+
+app.use('/api', limiter);
+app.use(morgan('dev'));
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb'}));
+
+// Data sanitization against NoSQL query injection
+app.use(expressMongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+// Prevent parameter pollution
+app.use(hpp({
+  whitelist: [
+    'duration',
+    'ratingsQuantity',
+    'ratingsAverage',
+    'maxGroupSize',
+    'difficulty',
+    'price'
+  ]
+}));
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.headers);
-  next()
-})
+  // console.log(req.headers);
+  next();
+});
 
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/users', userRouter)
-
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 // Handling unhandled routes in our app
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server! `))
-})
+  next(new AppError(`Can't find ${req.originalUrl} on this server! `));
+});
 
-app.use(globalErrorHandler)
-
+app.use(globalErrorHandler);
 
 module.exports = app;
