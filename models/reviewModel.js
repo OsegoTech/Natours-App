b89db1708c043d683a9d11f1ae3d9b33bd16ad57
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-07-07 15:44:28 +0300

    populating reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,6 +32,17 @@ const reviewSchema = new mongoose.Schema({
     },
 );
 
+reviewSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'tour',
+        select: 'name'
+    }).populate({
+        path: 'user',
+        select: 'name photo'
+    })
+    next();
+});
+
 // Preventing duplicate reviews
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
