210f2db7f1578a583a20b2208e8e2243d882c702
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-07-11 13:44:19 +0300

    calculating average rating on tours pt1

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,58 +1,80 @@
 const mongoose = require('mongoose');
-
-const reviewSchema = new mongoose.Schema({
+const Tour = require('./tourModel');
+const reviewSchema = new mongoose.Schema(
+  {
     review: {
-        type: String,
-        required: [true, 'Review cannot be empty!']
+      type: String,
+      required: [true, 'Review cannot be empty!'],
     },
     rating: {
-        type: Number,
-        min: 1,
-        max: 5
-
+      type: Number,
+      min: 1,
+      max: 5,
     },
     createdAt: {
-        type: Date,
-        default: Date.now(),
+      type: Date,
+      default: Date.now(),
     },
     tour: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Tour',
-        required: [true, 'Review must belong to a tour!']
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour!'],
     },
     user: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: [true, 'Review must belong to a user!']
-    }
-},
-    {
-        timestamps: true,
-        toJSON: { virtuals: true },
-        toObject: { virtuals: true }
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user!'],
     },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
 
 reviewSchema.pre(/^find/, function (next) {
-    // this.populate({
-    //     path: 'tour',
-    //     select: 'name'
-    // }).populate({
-    //     path: 'user',
-    //     select: 'name photo'
-    // })
+  // this.populate({
+  //     path: 'tour',
+  //     select: 'name'
+  // }).populate({
+  //     path: 'user',
+  //     select: 'name photo'
+  // })
 
-     this.populate({
-        path: 'user',
-        select: 'name photo'
-    })
-    next();
+  this.populate({
+    path: 'user',
+    select: 'name photo',
+  });
+  next();
 });
 
-// Preventing duplicate reviews
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    { 
+        $match: { tour: tourId },
+
+    },
+    {
+        $group: {
+            _id: '$tour',
+            nRating: { $sum: 1 },
+            avgRating: { $avg: '$rating' }
+        }
+    }
+    ]);
+    console.log(stats);
+    await Tour.findByIdAndUpdate(tourId, {
+        ratingsQuantity: stats[0].nRating,
+        ratingsAverage: stats[0].avgRating
+    })
+};
+
+reviewSchema.post('save', function () {
+    // this points to current review
+    this.constructor.calcAverageRatings(this.tour);
+    // next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 module.exports = Review;
-
-// POST /tour/234fad4/reviews
-// GET /tour/234fad4/reviews
-// GET /tour/234fad4/reviews/987dfg
