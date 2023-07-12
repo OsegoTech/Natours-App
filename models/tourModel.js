b410fd4fa36f6f25efa261eb153a04cbf84ed71f
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-07-12 23:06:18 +0300

    calculating distances

--- models/tourModel.js
+++ models/tourModel.js
@@ -181,11 +181,11 @@ tourSchema.post(/^find/, function(docs, next) {
 
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function(next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true}}})
-  console.log(this.pipeline());
-  next()
-})
+// tourSchema.pre('aggregate', function(next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true}}})
+//   console.log(this.pipeline());
+//   next()
+// })
 
 const Tour = mongoose.model('Tour', tourSchema)
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -30,6 +30,8 @@ router
 // /tours-within?distance=233&center=-40,45&unit=mi
 // /tours-within/233/center/-40,45/unit/mi
 
+router.route('/distances/:latlng/unit/:unit').get(tourController.getDistances);
+
 router
   .route('/')
   .get(tourController.getAllTours)
