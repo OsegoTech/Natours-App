8fd63267005bae6c9edd178434eae9269b7a58b0
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-07-16 15:24:20 +0300

    building tour tour page pt1

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,6 +3,6 @@ const viewController = require('../controllers/viewsController');
 const router = express.Router();
   
   router.get('/', viewController.getOverview);
-  router.get('/tour', viewController.getTour);
+  router.get('/tour/:slug', viewController.getTour);
   
 module.exports = router;
