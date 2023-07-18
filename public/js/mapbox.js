663ba48c85203ee2de1835b224b4e43d32b4a4a2
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2023-07-18 12:00:52 +0300

    building the login screen

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,2 +1,9 @@
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoib3NlZ290ZWNoIiwiYSI6ImNsazdjOXFpYTA3dDgzaHA2Y3MwbnRpOW4ifQ.Bk1_986RzuX77dRHGX79FA';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
   
   router.get('/', viewController.getOverview);
   router.get('/tour/:slug', viewController.getTour);
+  router.get('/login', viewController.getLoginForm);
   
 module.exports = router;
